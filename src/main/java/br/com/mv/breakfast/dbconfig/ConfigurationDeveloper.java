b5dd7fddc41a918175b1e3ce8cbf61f2a0b0497d
Hugo <hugohenrique20@gmail.com>
2025-04-25 00:54:23 -0300

    raw: parametriza config de conexao do banco de dados de dev e torna-a agnostica

--- src/main/java/br/com/mv/breakfast/dbconfig/ConfigurationDeveloper.java
+++ src/main/java/br/com/mv/breakfast/dbconfig/ConfigurationDeveloper.java
@@ -2,35 +2,38 @@ package br.com.mv.breakfast.dbconfig;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 @Profile("dev")
 public class ConfigurationDeveloper {
-	
+
 	@Bean
-    public DataSource dataSource(){
+    public DataSource dataSource(@Value("${spring.datasource.url}") String url,
+								 @Value("${spring.datasource.username}") String username,
+								 @Value("${spring.datasource.password}") String password,
+								 @Value("${spring.datasource.driver-class-name}") String driverClassName) {
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/breakfast_mv");
-        dataSource.setUsername("root");
-        dataSource.setPassword("hhsa1234");
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
-	
+
 	@Bean
-	public JpaVendorAdapter jpaVendorAdapter(){
+	public JpaVendorAdapter jpaVendorAdapter(@Value("${spring.jpa.database-platform}") String databasePlatform) {
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.MYSQL);
 		adapter.setShowSql(true);
 		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setDatabasePlatform(databasePlatform);
 		adapter.setPrepareConnection(true);
 		return adapter;
 	}
